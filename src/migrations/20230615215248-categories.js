a52d3ecd42c95757ba8755e5823ea0f7fe9195d6
PFonsecaFV <pfonsecafv@gmail.com>
2023-06-15 19:18:45 -0300

    :sparkles: Req 01 - Migration created for categories - Implementing function

---
+++ src/migrations/20230615215248-categories.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('categories');
+  }
+};
