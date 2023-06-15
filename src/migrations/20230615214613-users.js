8facee716884293dcbd3276aad1684b59402f2d7
PFonsecaFV <pfonsecafv@gmail.com>
2023-06-15 19:18:11 -0300

    :sparkles: Req 01 - Migration created for users - Implementing function

---
+++ src/migrations/20230615214613-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'display_name'
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
