4db5acb222e3cf52042d8d014ddacf720adf1a7c
PFonsecaFV <pfonsecafv@gmail.com>
2023-06-16 19:41:45 -0300

    :sparkles: Req 11 - Created PostCategory model with correct properties

---
+++ src/models/PostCategory.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', 
+  {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+  }, 
+  {
+    tableName: 'posts_categories',
+    timestamps: false,
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId'
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
