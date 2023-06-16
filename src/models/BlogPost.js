23248b98103ce27dab2663122a499c9f04c8f627
PFonsecaFV <pfonsecafv@gmail.com>
2023-06-16 19:24:02 -0300

    :sparkles: Req 10 - Created BlogPost model with correct properties

---
+++ src/models/BlogPost.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', 
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, 
+  {
+    tableName: 'blog_posts',
+    timestamps: false,
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId', as: 'user'
+    });
+  };
+
+  return BlogPost;
+};
