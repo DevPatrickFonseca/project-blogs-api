727b0d0b2bed17eef73b3285d957ed0ef04ddd1d
PFonsecaFV <pfonsecafv@gmail.com>
2023-06-16 17:19:45 -0300

    :sparkles: Req 05 - Create findAll Users on the Controller Layer - Implementing function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,21 @@
 const { userService } = require('../services');
 
+const getUsersController = async (req, res) => {
+  const { type, data } = await userService
+    .getUsers();
+
+  return res.status(type).json(data);
+};
+
 const createUserController = async (req, res) => {
   const { displayName, email, password, image } = req.body;
-  const { type, data } = await userService.postUser({ displayName, email, password, image });
+  const { type, data } = await userService
+    .postUser({ displayName, email, password, image });
 
   return res.status(type).json(data);
 };
 
 module.exports = {
   createUserController,
+  getUsersController,
 };
