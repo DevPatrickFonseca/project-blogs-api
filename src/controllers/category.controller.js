765af9f11fabc588e62ff4f3520d20c76fae3796
PFonsecaFV <pfonsecafv@gmail.com>
2023-06-16 19:06:30 -0300

    :sparkles: Req 09 - Create findAll Categories on the Controller Layer - Implementing function

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,5 +1,12 @@
 const { categoryService } = require('../services');
 
+const getCategoriesController = async (req, res) => {
+  const { type, data } = await categoryService
+    .getCategories();
+
+  return res.status(type).json(data);
+};
+
 const createCategoryController = async (req, res) => {
   const { name } = req.body;
   const { type, data } = await categoryService
@@ -10,4 +17,5 @@ const createCategoryController = async (req, res) => {
 
 module.exports = {
   createCategoryController,
+  getCategoriesController,
 };
