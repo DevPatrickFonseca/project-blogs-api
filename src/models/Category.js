cd7edf6ecb71a78f10fc6317a17d6bf346329d99
PFonsecaFV <pfonsecafv@gmail.com>
2023-06-16 17:47:37 -0300

    :sparkles: Req 07 - Created Category model with correct properties

---
+++ src/models/Category.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', 
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING,
+  }, 
+  {
+    tableName: 'categories',
+    timestamps: false,
+    underscored: true,
+  });
+
+  return Category;
+};
