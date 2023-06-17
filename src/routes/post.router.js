e1003fa72b0816818b212659e52cfd8f16bf9844
PFonsecaFV <pfonsecafv@gmail.com>
2023-06-17 19:38:52 -0300

    :art: Req 12 - Implementing POST /post Route Validation

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -5,8 +5,11 @@ const routers = express.Router();
 const { postController } = require('../controllers');
 
 const { tokenValidator } = require('../middlewares/tokenValidator');
+const { postValidator } = require('../middlewares/postValidator');
 
 routers.get('/', tokenValidator, postController.getPostController);
 routers.get('/:id', tokenValidator, postController.getPostByIdController);
+
+routers.post('/', tokenValidator, postValidator, postController.createPostController);
 
 module.exports = routers;
