8e126f909382d79b62436645f4bef31be7bec9ad
PFonsecaFV <pfonsecafv@gmail.com>
2023-06-17 13:15:30 -0300

    :sparkles: Req 14 - Create findById Post on the Service Layer - Implementing function

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -18,6 +18,25 @@ const getPosts = async () => {
   return { type: 200, data: allPosts };
 };
 
+const getPostById = async (id) => {
+  const postId = await BlogPost.findByPk(id, {
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+
+  if (!postId) {
+    return { 
+      type: 404, 
+      data: { message: 'Post does not exist' }, 
+    };
+  }
+  
+  return { type: 200, data: postId };
+};
+
 module.exports = {
   getPosts,
+  getPostById,
 };
